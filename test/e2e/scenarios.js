43068eb289da3fa81add9bf91a4fe8fb0c00d4e6
Chris Nelson <chris@gaslightsoftware.com>
2013-09-16 15:22:41 -0400

    e2e spec failing oddly

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,44 +2,19 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('cookbook', function() {
 
   beforeEach(function() {
     browser().navigateTo('../../app/index.html');
   });
 
+  describe('Listing Recipes', function() {
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
-  });
-
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
+    it('should display recipes', function() {
+      expect(element('#recipe_list_view li').text()).
+        toMatch(/Pancakes/);
     });
 
   });
 
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
