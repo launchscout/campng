8917155312dddc2201e026312d0f18ff43aeb509
Chris Nelson <chris@gaslightsoftware.com>
2013-09-16 17:53:34 -0400

    Added template, routing. Ready for lab

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,3 +3,6 @@
 angular.module("cookbook").controller("RecipesCtrl", function ($scope) {
   $scope.recipes = [{title: "Pancakes"}, {title: "Waffles"}];
 });
+
+angular.module("cookbook").controller("RecipeShowCtrl", function ($scope) {
+});
