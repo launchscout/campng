7cc983c165467637c4d881d47d073dca184f0828
Chris Nelson <chris@gaslightsoftware.com>
2013-09-16 16:50:52 -0400

    Failing view recipe e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
 describe('cookbook', function() {
@@ -13,6 +11,17 @@ describe('cookbook', function() {
     it('should display recipes', function() {
       expect(element('#recipe_list_view li').text()).
         toMatch(/Pancakes/);
+    });
+
+  });
+
+  describe("Viewing a recipe", function() {
+    beforeEach(function() {
+      browser().navigateTo('#recipes/1');
+    });
+    it('should display title and description', function() {
+      expect(element('#recipe_view dd').text()).
+        toMatch(/Mmm.. delicious/);
     });
 
   });
