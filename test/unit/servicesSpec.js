28f59fa4aa323ff4061d8994bf4430ffd720366b
Chris Nelson <chris@gaslightsoftware.com>
2013-09-16 22:50:46 -0400

    failing Recipe service spec

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -1,14 +1,23 @@
-// 'use strict';
-
 // /* jasmine specs for services go here */
 
-// describe('service', function() {
-//   beforeEach(module('myApp.services'));
+describe('service', function() {
+  beforeEach(module('cookbook'));
 
+  describe('version', function() {
+    it('should return current version', inject(function(version) {
+      expect(version).toEqual('0.1');
+    }));
+  });
 
-//   describe('version', function() {
-//     it('should return current version', inject(function(version) {
-//       expect(version).toEqual('0.1');
-//     }));
-//   });
-// });
+  describe("Recipe", function() {
+    beforeEach(inject(function(Recipe) {
+      Recipe.recipes = [
+        {id: 1, title: "Waffles", description: "Yummy"},
+        {id: 2, title: "Pancakes", description: "Good"}
+      ];
+    }));
+    it("should find by id", function() {
+      expect(Recipe.find(1).title).toEqual("Yummy");
+    });
+  });
+});
