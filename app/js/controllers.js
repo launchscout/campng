7195a9e1688179ef7cdf0f64f82d7e072b235b75
Chris Nelson <chris@gaslightsoftware.com>
2013-09-16 23:42:20 -0400

    passing e2e spec

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,6 @@ angular.module("cookbook").controller("RecipesCtrl", function ($scope) {
   $scope.recipes = [{title: "Pancakes"}, {title: "Waffles"}];
 });
 
-angular.module("cookbook").controller("RecipeShowCtrl", function ($scope) {
-  $scope.recipe = {title: "Pancackes", description: "Mmm.. delicious"};
+angular.module("cookbook").controller("RecipeShowCtrl", function ($scope, Recipe, $routeParams) {
+  $scope.recipe = Recipe.find($routeParams.recipeId);
 });
