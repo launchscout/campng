d13d2259670608b7d8135c5a1ccb691b10f2436b
Chris Nelson <chris@gaslightsoftware.com>
2014-03-13 19:51:17 -0400

    failing spec for markdown directive

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -1,19 +1,33 @@
-// 'use strict';
+'use strict';
 
-// /* jasmine specs for directives go here */
+/* jasmine specs for directives go here */
 
-// describe('directives', function() {
-//   beforeEach(module('myApp.directives'));
+describe('directives', function() {
+  beforeEach(module('cookbook'));
 
-//   describe('app-version', function() {
-//     it('should print current version', function() {
-//       module(function($provide) {
-//         $provide.value('version', 'TEST_VER');
-//       });
-//       inject(function($compile, $rootScope) {
-//         var element = $compile('<span app-version></span>')($rootScope);
-//         expect(element.text()).toEqual('TEST_VER');
-//       });
-//     });
-//   });
-// });
+  describe('app-version', function() {
+    it('should print current version', function() {
+      module(function($provide) {
+        $provide.value('version', 'TEST_VER');
+      });
+      inject(function($compile, $rootScope) {
+        var element = $compile('<span app-version></span>')($rootScope);
+        expect(element.text()).toEqual('TEST_VER');
+      });
+    });
+  });
+
+  describe('markdownPreview previews markdown', function() {
+    beforeEach(inject(function($rootScope) {
+      $rootScope.text = "*hi* there";
+    }));
+    it('should print current version', function() {
+      inject(function($compile, $rootScope) {
+        var element = $compile('<div markdown-preview text="text"></div>')($rootScope);
+        $rootScope.$apply();
+        expect(element.find("em").html()).toEqual('hi');
+      });
+    });
+  });
+
+});
