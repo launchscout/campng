29dfbcb3fd84ff59a5e57ecad7adb9c97ea44aa8
Chris Nelson <chris@gaslightsoftware.com>
2014-04-12 20:14:42 -0400

    recipes need an id

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,7 @@
 /* Controllers */
 
 angular.module("cookbook").controller("RecipesCtrl", function ($scope) {
-  $scope.recipes = [{title: "Pancakes"}, {title: "Waffles"}];
+  $scope.recipes = [{id: 1, title: "Pancakes"}, {id: 2, title: "Waffles"}];
 });
 
 angular.module("cookbook").controller("RecipeShowCtrl", function ($scope) {
